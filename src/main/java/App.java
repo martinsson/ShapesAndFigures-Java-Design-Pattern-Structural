a30bcfd6edd7ae3644eeece4739b9f0506a6a5e2
Johan Martinsson <martinsson.johan@gmail.com>
2016-04-25 15:08:51 +0200

    show the problem

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import com.sun.prism.shader.DrawCircle_LinearGradient_PAD_Loader;
 import shapes.Position;
 
 public class App {
@@ -7,23 +8,44 @@ public class App {
     }
 
     public void runIt() {
-        int height = 44;
-        int width = 30;
-        Dimensions dimensions = new Dimensions(width, height);
-        Rectangle rectangle = new Rectangle(dimensions);
 
         int yposition = 0;
         int xposition = 0;
         Position position = new Position(yposition, xposition);
-        drawShape(position, rectangle);
 
 
-        drawShape(position, new Circle(10));
+
+        int height = 44;
+        int width = 30;
+        Dimensions dimensions = new Dimensions(width, height);
+
+
+        Rectangle rectangle = new Rectangle(dimensions);
+        Circle circle = new Circle(10);
+        
+
+        draw(circle, position, dimensions);
+        
+
     }
 
-    private void drawShape(Position position, Shape rectangle) {
+    private void draw(Shape shape, Position position, Dimensions dimensions) {
+        shape.drawAt(position);
+        if (shape instanceof Circle) {
+            drawCircle(position);
+        } else if (shape instanceof Rectangle) {
+            drawRectangle(position, dimensions);
+        }
+    }
 
-        rectangle.drawAt(position);
+    private void drawRectangle(Position position, Dimensions dimensions) {
+        System.out.print("Rectangle with dimensions "  + dimensions.toString() + " ");
+        System.out.println("Drawn at " + position.toString());
+    }
+
+    private void drawCircle(Position position) {
+        System.out.print("Circle ");
+        System.out.println("Drawn at " + position.toString());
     }
 
 
